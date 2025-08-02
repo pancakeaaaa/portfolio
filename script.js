04ac0411a01b1a1f97f2f1a64eb1b19cd8c78173
pancakeaaaa <wirakusumahrae@gmail.com>
2025-08-02 14:28:59 +0800

    main

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-let mybutton = document.getElementById("myBtn");
+  let myBtn = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+  window.onscroll = function() {
+    scrollFunction();
+  };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      myBtn.style.display = "block";
+    } else {
+      myBtn.style.display = "none";
+    }
   }
-}
 
-function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0; 
-} 
+  function topFunction() {
+    document.body.scrollTop = 0; // for Safari
+    document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE
+  }
+</script>
